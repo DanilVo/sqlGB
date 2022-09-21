5b96de15438e2a483e754504ed30af3fd15b37ee
DanilV <Dani736@gmail.com>
2022-09-21 21:02:00 +0300

    seminar 1 HW

---
+++ seminar1.sql
@@ -0,0 +1,7 @@
+#SELECT * FROM myfirst.smartphones;
+#SELECT Manufacter,price FROM smartphones WHERE ProductCount > 2;
+#Select * from smartphones where Manufacter = 'Samsung';
+#select * from smartphones where ProductName REGEXP '[I]';
+#select * from smartphones where ProductName REGEXP '[S]';
+#select * from smartphones where ProductName REGEXP '[0-9]';
+#select * from smartphones where ProductName REGEXP '[8]';
